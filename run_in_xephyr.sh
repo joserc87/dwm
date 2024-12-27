a466e13602488652a14ba0afe3051c2a9b88d162
Jose Ramon Cano <joserc87@gmail.com>
2024-12-28 00:29:57 +0100

    Watchdog Xephyr

--- run_in_xephyr.sh
+++ run_in_xephyr.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 make clean all
 DSP=:2
-Xephyr -br -ac -reset -screen 800x600 "$DSP" &
+Xephyr -br -ac -reset -screen 2850x1750 "$DSP" &
 export DISPLAY="$DSP"
 sleep 1
 echo running dwm
-./dwm
+while true; do
+    ./dwm || break
+done
